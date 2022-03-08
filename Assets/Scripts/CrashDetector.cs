2de748e6b4292af62c1c468e53cce8a39e7705fd
kaizersoze90 <onurkiris05@gmail.com>
2022-03-08 17:08:42 +0300

    Added some additional features

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float invokeDelay = 2f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSound;
+
+    bool hasCrashed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("You'r dead!");
+        if (other.tag == "Ground" && !hasCrashed)
+        {
+            FindObjectOfType<PlayerController>().DisableControl();
+            GetComponent<AudioSource>().PlayOneShot(crashSound, 1f);
+            crashEffect.Play();
+            Invoke("ReloadScene", invokeDelay);
+            hasCrashed = true;
+        }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
